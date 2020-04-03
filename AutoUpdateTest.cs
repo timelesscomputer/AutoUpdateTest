56d707a7b9a3acfec8005233202bf8b7c1510d64
Queuete <->
2020-04-03 11:46:59 +0200

    Initial commit

---
+++ AutoUpdateTest.cs
@@ -0,0 +1,27 @@
+﻿using ExileCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdateTest
+{
+    public class AutoUpdateTest : BaseSettingsPlugin<AutoUpdateTestSettings>
+    {
+        public override bool Initialise()
+        {
+            var path = "Textfile1.txt";
+            var exists = File.Exists(path);
+            DebugWindow.LogMsg($"{path} -> {exists}");
+            var content = File.ReadAllText(path);
+            DebugWindow.LogMsg($"{path} content -> {content}");
+            return true;
+        }
+
+        public override void Render()
+        {
+        }
+    }
+}
---
+++ AutoUpdateTestSettings.cs
@@ -0,0 +1,15 @@
+﻿using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoUpdateTest
+{
+    public class AutoUpdateTestSettings : ISettings
+    {
+        public ToggleNode Enable { get; set; }
+    }
+}
